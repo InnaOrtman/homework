0243bf395ca10f33344a064dc22c14ba063d90b1
InnaOrtman <innalevko1987@gmail.com>
2021-05-22 13:08:51 +0300

    test

--- task_1.py
+++ task_1.py
@@ -1,18 +1,16 @@
-'''
-Write a Python program to get a string made of the first 2 and the last 2 chars from a given string.
-If the string length is less than 2, return instead of the empty string.
-Sample String: 'helloworld'
-Expected Result : 'held'
-Sample String: 'my'
-Expected Result : 'mymy'
-Sample String: ' x'
-Expected Result: Empty String
-'''
-s1 = 'helloworld'
-print(s1[0:2]+s1[-2:len(s1)])
+# Write a Python program to get a string made of the first 2 and the last 2 chars from a given string.
+# If the string length is less than 2, return instead of the empty string.
+# Sample String: 'helloworld'
+# Expected Result : 'held'
+# Sample String: 'my'
+# Expected Result : 'mymy'
+# Sample String: ' x'
+# Expected Result: Empty String
 
-s2 = 'my'
-print(s2*2)
+s1 = 'my'
 
-s3 = 'x'
-print(s3[0:-1])
+if len(s1) < 2:
+    print(' ')
+else:
+    print(s1[:2]+s1[-2:])
+
