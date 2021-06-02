aa71f74c06e322a1b6045f27f28b5c90e99f0949
InnaOrtman <innalevko1987@gmail.com>
2021-06-02 12:29:10 +0300

    test

--- урок 6/task_3.py
+++ урок 6/task_3.py
@@ -1,11 +1,9 @@
 # Use a list comprehension to make a list containing tuples (i, j)
 # where `i` goes from 1 to 10 and `j` is corresponding to `i` squared.
 
-numbers = []
+numbers = [(x, x * x) for x in range(1, 10)]
 
-for x in range(10):
-    c = x ** 2
-    numbers.append(c)
 
 print(numbers)
+
 
